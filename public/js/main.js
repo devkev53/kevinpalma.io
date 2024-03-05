b24ad779942b5d15f57f9f5e4052613f6ce74cf9
Kevin Palma <kapalma05@gmail.com>
2024-03-05 11:22:09 -0600

    :bug: Fix bugs in js archives
    Fix bugs in js archives

---
+++ public/js/main.js
@@ -0,0 +1,54 @@
+let isDebug = true;
+
+const checkModalState = () => {
+  if (isDebug) {
+    return window.localStorage.getItem("closeModal") ?? false;
+  }
+};
+
+// -*-*-*-*-*-*-*- BURGUER BUTTON FUNCION -*-*-*-*-*-*-*-
+export function burguerBtnFunction() {
+  const burguerBtn = document.querySelector(".burguer_btn");
+
+  const linksMenu = document.querySelectorAll(".menu_link");
+
+  burguerBtn.addEventListener("click", () => {
+    burguerBtn.classList.toggle("openMenu");
+  });
+
+  linksMenu.forEach((link) => {
+    link.addEventListener("click", () => {
+      burguerBtn.classList.remove("openMenu");
+    });
+  });
+}
+
+burguerBtnFunction();
+
+// -*-*-*-*-*-*-*- MODAL CLOSE FUNCION -*-*-*-*-*-*-*-
+export function closeModal(state) {
+  const btnClose = document.querySelector("#btnClose");
+  const btnCloseEnter = document.querySelector(".enterCloseBtn");
+  const modal = document.querySelector("#modal");
+  console.log(state());
+
+  if (state()) {
+    modal.classList.add("close");
+  }
+
+  const setInLocalStorage = () => {
+    window.localStorage.setItem("closeModal", true);
+  };
+
+  btnClose.addEventListener("click", function (e) {
+    modal.classList.add("close");
+    setInLocalStorage();
+  });
+
+  btnCloseEnter.addEventListener("click", function (e) {
+    modal.classList.add("close");
+    setInLocalStorage();
+  });
+}
+
+closeModal(checkModalState);
