ac2468d315c5dc8e4433ae5e1fce810976adc932
Kevin Palma <root@DESKTOP-9D5BBSU.>
2024-03-04 21:47:08 -0600

    :construction: Work in hero
    work in hero

---
+++ js/burguerBtn.js
@@ -0,0 +1,5 @@
+const burguerBtn = document.querySelector(".burguer_btn");
+
+burguerBtn.addEventListener("click", () => {
+  burguerBtn.classList.toggle("openMenu");
+});
---
+++ js/main.js
@@ -0,0 +1,5 @@
+import { drawMyLogo } from "../js/icons/kpLogo";
+import { drawSocialIcons } from "../js/icons/socialIcons";
+
+drawMyLogo();
+drawSocialIcons();
