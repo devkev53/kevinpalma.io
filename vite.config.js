dfca7a17b3885a2cd75bbd53c1f0d50da0441a11
Kevin Palma <kapalma05@gmail.com>
2025-03-22 09:12:44 -0600

    :pencil2: Change vite config
    change vite config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://devkev53.github.io/kevinpalma.io'
+  base: ''
 })
