61966e3409595116c9acc518c9f4e491ee5b9385
Devkev53 <kapalma05@gmail.com>
2026-05-23 10:08:56 -0600

    ♻️ Reset all project
    Reset

--- src/store/langStore.js
+++
@@ -1,6 +0,0 @@
-import { create } from "zustand";
-
-export const langStore = create((set) => ({
-  langCode: { label: "English", code: "en" },
-  setLangCode: (langCode) => set({ langCode }),
-}));
